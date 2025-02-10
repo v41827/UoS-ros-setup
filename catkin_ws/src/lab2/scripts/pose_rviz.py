e33e54a699ea71115d497ad541ae2e815decb198
frankcholula <lu.phrank@gmail.com>
2025-02-10 13:41:20 +0000

    added rviz code.

--- catkin_ws/src/lab2/scripts/pose_rviz.py
+++ catkin_ws/src/lab2/scripts/pose_rviz.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import PoseStamped, Quaternion
-msg = PoseStamped()
+from turtlesim.msg import Pose
+import tf
 
 def callback(data):
+    msg = PoseStamped()
     msg.header.frame_id = "map"
-    msg.ehader.seq = 0
+    msg.header.seq = 0
     msg.header.stamp = rospy.Time.now()
 
     q = tf.transformations.quaternion_from_euler(0, 0, data.theta)
     msg.pose.orientation = Quaternion(*q)
     msg.pose.position.x = data.x
     msg.pose.position.y = data.y
+    
+    # Publish the transformed pose
+    pub.publish(msg)
 
-if __name__ = '__main__':
-    rospy.init_node('pose_rviz_python')
-
+if __name__ == '__main__':
+    try:
+        # Initialize the ROS node
+        rospy.init_node('pose_rviz', anonymous=True)
+        global pub
+        pub = rospy.Publisher('turtle1/pose_stamped', PoseStamped, queue_size=10)
+        rospy.Subscriber('/turtle1/pose', Pose, callback)
+        rospy.spin()
+        
+    except rospy.ROSInterruptException:
+        pass
